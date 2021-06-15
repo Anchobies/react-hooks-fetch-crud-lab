1070bffb01a61bf42468e4cacafa669a849a10f4
Anchobies <anthonychung97@gmail.com>
2021-06-15 12:58:58 -0400

    Completed assignment

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,20 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ updateQuestions, handleDeleteQuestion, questions }) {
+  const questionsArray = questions.map(question => {
+    return <QuestionItem 
+      key={question.id} 
+      updateQuestions={updateQuestions} 
+      handleDeleteQuestion={handleDeleteQuestion} 
+      question={question} 
+    />
+  })
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionsArray}</ul>
     </section>
   );
 }
